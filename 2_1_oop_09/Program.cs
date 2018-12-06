2cace058ffd659a1d03c03bfe7ab3e6e65e3c583
Alexey Astakhov <am.astakhov@gmail.com>
2018-12-07 00:04:08 +0300

    болванка для 9-й лабораторной

---
+++ 2_1_oop_09/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ns_2_1_oop {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ 2_1_oop_09/User.cs
@@ -0,0 +1,5 @@
+﻿using System;
+
+namespace ns_2_1_oop {
+    class User {}
+}
