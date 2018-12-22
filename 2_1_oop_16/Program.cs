f8f50f27cf63ec1d7f6b3cb6503c8495034bd80c
Alexey Astakhov <am.astakhov@gmail.com>
2018-12-22 13:51:58 +0300

    заготовка проекта для ООП-16

---
+++ 2_1_oop_16/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace ns_2_1_oop {
+    class Program {
+        static void Main(string[] args) {
+            try {
+                // 1.
+            } catch (Exception ex) {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+}
