e4606fa8d3ee27f632abe3bce49c86e2381d55dd
Alexey Astakhov <am.astakhov@gmail.com>
2018-12-26 12:50:51 +0300

    ООП-16 раскомментил. и 9я ксис - недоделанная

--- 2_1_oop_16/Program.cs
+++ 2_1_oop_16/Program.cs
@@ -13,100 +13,100 @@ namespace ns_2_1_oop {
                 CancellationToken token			  = cancelTokenSource.Token;
 
                 // 1.
-                // Console.WriteLine("Запуск задачи");
-                // Task<List<int> > task1 = Task<List<int> >.Run(() => eratosfen(200_000, token));
-                // Thread.Sleep(1000);
-                // Console.WriteLine($"Статус задачи: зарвешена ли? = {task1.IsCompleted}");
+                Console.WriteLine("Запуск задачи");
+                Task<List<int> > task1 = Task<List<int> >.Run(() => eratosfen(200_000, token));
+                Thread.Sleep(1000);
+                Console.WriteLine($"Статус задачи: зарвешена ли? = {task1.IsCompleted}");
 
-                // task1.Wait();
-                // Console.WriteLine($"Количество простых чисел: {task1.Result.Count}");
+                task1.Wait();
+                Console.WriteLine($"Количество простых чисел: {task1.Result.Count}");
 
-                // // 2.
-                // Console.WriteLine("Запуск задачи");
-                // Task<List<int> > task2 = Task<List<int> >.Run(() => eratosfen(10_000_000, token));
-                // Thread.Sleep(1000);
-                // Console.WriteLine($"Статус задачи: зарвешена ли? = {task2.IsCompleted}");
+                // 2.
+                Console.WriteLine("Запуск задачи");
+                Task<List<int> > task2 = Task<List<int> >.Run(() => eratosfen(10_000_000, token));
+                Thread.Sleep(1000);
+                Console.WriteLine($"Статус задачи: зарвешена ли? = {task2.IsCompleted}");
 
-                // Console.WriteLine("Завершаем задачу");
-                // cancelTokenSource.Cancel();
-                // task2.Wait();
-                // Console.WriteLine("Задача завершена");
+                Console.WriteLine("Завершаем задачу");
+                cancelTokenSource.Cancel();
+                task2.Wait();
+                Console.WriteLine("Задача завершена");
 
-                // // 3.
-                // Task<int>[] tasks = new Task<int>[3] {
-                //     new Task<int>(() => { Thread.Sleep(100); return 100; }),
-                //     new Task<int>(() => { Thread.Sleep(200); return 200; }),
-                //     new Task<int>(() => { Thread.Sleep(300); return 300; })
-                // };
+                // 3.
+                Task<int>[] tasks = new Task<int>[3] {
+                    new Task<int>(() => { Thread.Sleep(100); return 100; }),
+                    new Task<int>(() => { Thread.Sleep(200); return 200; }),
+                    new Task<int>(() => { Thread.Sleep(300); return 300; })
+                };
 
-                // // 4.1
-                // foreach (var t in tasks) {
-                //     t.Start();
-                // }
-                // Task<int> sumTask = new Task<int>(() => tasks[0].Result + tasks[1].Result + tasks[2].Result);
-                // Task nextTesk	  = sumTask.ContinueWith((t) => {
-                //     Console.WriteLine($"Результат сложения: {t.Result}");
-                // });
-                // sumTask.Start();
-                // nextTesk.Wait();
+                // 4.1
+                foreach (var t in tasks) {
+                    t.Start();
+                }
+                Task<int> sumTask = new Task<int>(() => tasks[0].Result + tasks[1].Result + tasks[2].Result);
+                Task nextTesk	  = sumTask.ContinueWith((t) => {
+                    Console.WriteLine($"Результат сложения: {t.Result}");
+                });
+                sumTask.Start();
+                nextTesk.Wait();
 
-                // // 4.2
-                // Task<int> sumTask2 = new Task<int>(() => tasks[0].Result + tasks[1].Result + tasks[2].Result);
-                // Task nextTesk2	   = new Task(() => {
-                //     Console.WriteLine($"Результат сложения: {sumTask2.GetAwaiter().GetResult()}");
-                // });
-                // sumTask2.Start();
-                // nextTesk2.Start();
-                // nextTesk2.Wait();
+                // 4.2
+                Task<int> sumTask2 = new Task<int>(() => tasks[0].Result + tasks[1].Result + tasks[2].Result);
+                Task nextTesk2	   = new Task(() => {
+                    Console.WriteLine($"Результат сложения: {sumTask2.GetAwaiter().GetResult()}");
+                });
+                sumTask2.Start();
+                nextTesk2.Start();
+                nextTesk2.Wait();
 
-                // // 5.
-                // Console.WriteLine("Генерирование случайного массива...");
-                // Random	  random   = new Random();
-                // double[]  data	   = new double[500_000_000];
-                // Stopwatch watchSeq = new Stopwatch();
-                // watchSeq.Start();
-                // for (int i = 0; i < data.Length; i++) {
-                //     data[i] = random.NextDouble() * random.NextDouble();
-                // }
-                // watchSeq.Stop();
-                // Console.WriteLine($"Последовательное решение заняло времени: {watchSeq.Elapsed}");
+                // 5.
+                Console.WriteLine("Генерирование случайного массива...");
+                Random	  random   = new Random();
+                double[]  data	   = new double[500_000_000];
+                Stopwatch watchSeq = new Stopwatch();
+                watchSeq.Start();
+                for (int i = 0; i < data.Length; i++) {
+                    data[i] = random.NextDouble() * random.NextDouble();
+                }
+                watchSeq.Stop();
+                Console.WriteLine($"Последовательное решение заняло времени: {watchSeq.Elapsed}");
 
-                // Stopwatch watchPar = new Stopwatch();
-                // watchPar.Start();
-                // Parallel.For(1, 10, (x) => {
-                //     Random        random2 = new Random();
-                //     int start		  = x * (data.Length / 10);
-                //     int end		  = (x + 1) * (data.Length / 10);
-                //     for (int i = start; i < end; i++) {
-                //         data[i] = random2.NextDouble() * random2.NextDouble();
-                //     }
-                // });
-                // watchPar.Stop();
-                // Console.WriteLine($"Распараллеленное решение заняло времени: {watchPar.Elapsed}");
+                Stopwatch watchPar = new Stopwatch();
+                watchPar.Start();
+                Parallel.For(1, 10, (x) => {
+                    Random        random2 = new Random();
+                    int start		  = x * (data.Length / 10);
+                    int end		  = (x + 1) * (data.Length / 10);
+                    for (int i = start; i < end; i++) {
+                        data[i] = random2.NextDouble() * random2.NextDouble();
+                    }
+                });
+                watchPar.Stop();
+                Console.WriteLine($"Распараллеленное решение заняло времени: {watchPar.Elapsed}");
 
-                // Stopwatch watchPar2 = new Stopwatch();
-                // watchPar2.Start();
-                // Parallel.For(1, 10, (x) => {
-                //     int start = x * (data.Length / 10);
-                //     int end   = (x + 1) * (data.Length / 10);
+                Stopwatch watchPar2 = new Stopwatch();
+                watchPar2.Start();
+                Parallel.For(1, 10, (x) => {
+                    int start = x * (data.Length / 10);
+                    int end   = (x + 1) * (data.Length / 10);
 
-                //     for (int i = start; i < end; i++) {
-                //         data[i] = random.NextDouble() * random.NextDouble();
-                //     }
-                // });
-                // watchPar2.Stop();
-                // Console.WriteLine($"Распараллеленное решение2 заняло времени: {watchPar2.Elapsed}");
+                    for (int i = start; i < end; i++) {
+                        data[i] = random.NextDouble() * random.NextDouble();
+                    }
+                });
+                watchPar2.Stop();
+                Console.WriteLine($"Распараллеленное решение2 заняло времени: {watchPar2.Elapsed}");
 
-                // // 6.
-                // Parallel.Invoke(() => { Console.WriteLine($"Выполняется задача {Task.CurrentId}");
-                //                         int x	    = 20;
-                //                         long result = 1;
-                //                         for (int i = 1; i <= x; i++) {
-                //                             result *= i;
-                //                         }
-                //                         Console.WriteLine("Факториал числа {0} равен {1}", x, result); },
-                //                 () => { Console.WriteLine($"Выполняется задача {Task.CurrentId}");
-                //                         Thread.Sleep(1000); });
+                // 6.
+                Parallel.Invoke(() => { Console.WriteLine($"Выполняется задача {Task.CurrentId}");
+                                        int x	    = 20;
+                                        long result = 1;
+                                        for (int i = 1; i <= x; i++) {
+                                            result *= i;
+                                        }
+                                        Console.WriteLine("Факториал числа {0} равен {1}", x, result); },
+                                () => { Console.WriteLine($"Выполняется задача {Task.CurrentId}");
+                                        Thread.Sleep(1000); });
 
                 // 7.
                 Task[] suppliers = new Task[] {
@@ -150,7 +150,7 @@ namespace ns_2_1_oop {
                 Console.WriteLine("Выполнение основного потока 4 сек");
                 Thread.Sleep(2000);
                 Console.WriteLine("Выполнение основного потока 6 сек");
-                Console.WriteLine(		   "Прерываем задачу");
+                Console.WriteLine("Прерываем задачу");
                 cancelTokenSource.Cancel();
                 Console.WriteLine("Выполнение основного потока завершено");
             } catch (Exception ex) {
